c10b432a615256eda208dfe20bb03d67aaac32a4
fgil1982 <fgilborges1982@gmail.com>
2025-05-27 10:07:14 -0300

    Adicionando funcionalidade mostrar mensagem

---
+++ Projeto1/cadastro.js
@@ -0,0 +1,11 @@
+function signUP(email){
+return email;
+}
+
+// pegando email e cadastrando na plataforma
+
+function mostraMensagem(){
+    return 'Bem vindo ao sistema';
+}
+
+// Adicionando função de mensagem
